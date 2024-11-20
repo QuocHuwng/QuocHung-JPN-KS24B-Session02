5bae7029e3150aeced7850d051526b46a645e5ee
QuocHuwng <nguyenhung2952006@gmail.com>
2024-11-20 23:16:42 +0700

    Add files via upload

---
+++ Session02_Ex5.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+	int a = 6;
+	int b = 5; 
+	printf("Chu vi hinh chu nhat la : (a + b)*2 = %d\n", (a + b)*2);
+	printf("Dien tich hinh chu nhat la : a * b = %d", a * b); 
+}
+
+
