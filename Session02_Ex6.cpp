cccc52fa1edceaa7789c33753256829346d43f7a
QuocHuwng <nguyenhung2952006@gmail.com>
2024-11-21 00:00:06 +0700

    Add files via upload

---
+++ Session02_Ex6.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+	float PI = 3.14;
+	int r = 2;
+	printf("Chu vi hinh tron : 2*PI*r = %f\n", 2*PI*r);
+	printf("Dien tich hinh tron : PI*r*r = %f", PI*r*r);
+}
+
+
